f9cd61b919ac416b88fbc4d777680246a16a0fa3
AleksZelenchuk <zelenchuk.oleks@gmail.com>
2018-08-06 13:37:09 +0300

    Update registration.php

--- CustomScripts/registration.php
+++ CustomScripts/registration.php
@@ -5,6 +5,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Rbo_CustomScripts',
+    'Namespace_CustomScripts',
     __DIR__
 );
